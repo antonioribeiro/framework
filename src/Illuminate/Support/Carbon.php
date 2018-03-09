ebf3904d1b128e5ddaefc99c0baec6523f869986
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-03-09 12:27:22 -0300

    Add typehint
    To support this change in Carbon: https://github.com/briannesbitt/Carbon/commit/08bf9b9ef1a8d6b09419177e9aba49319e70180e#diff-c060a4f07dfb6bce7015fa79a21b6963R3854

--- src/Illuminate/Support/Carbon.php
+++ src/Illuminate/Support/Carbon.php
@@ -47,13 +47,14 @@ class Carbon extends BaseCarbon implements JsonSerializable
     }
 
     /**
-     * Create a new Carbon instance based on the given state array.
+     * The __set_state handler.
      *
-     * @param  array  $array
+     * @param array $state
+     *
      * @return static
      */
-    public static function __set_state($array)
+    public static function __set_state(array $state)
     {
-        return static::instance(parent::__set_state($array));
+        return static::instance(parent::__set_state($state));
     }
 }
